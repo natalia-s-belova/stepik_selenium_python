818c3e1a4d8ad7c3bd690b6728438f29a46fd63a
natalia-s-belova <natalya.s.belova@gmail.com>
2023-10-14 21:06:59 +0400

    Add tests for Basket

---
+++ stepik_selenium_python/pages/basket_page.py
@@ -0,0 +1,16 @@
+from stepik_selenium_python.pages.locators import BasketPageLocators
+from stepik_selenium_python.pages.base_page import BasePage
+
+
+class BasketPage(BasePage):
+    def should_be_empty(self):
+        self.should_have_no_products()
+        self.should_have_empty_basket_message()
+
+    def should_have_no_products(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_CONTENT), 'Something is in the basket!'
+
+    def should_have_empty_basket_message(self):
+        text = 'Your basket is empty. Continue shopping'
+        visible_message = self.browser.find_element(*BasketPageLocators.TOP_MESSAGE).text
+        assert visible_message == text, f'Expected text:{text}, actual text: {visible_message}'
