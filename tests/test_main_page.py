097a7f6fa2c50eb3b86217435734b56769bc97a3
natalia-s-belova <natalya.s.belova@gmail.com>
2023-10-14 13:05:45 +0400

    Add storage for future PageObject realization and first methods

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -2,9 +2,13 @@ from selenium.webdriver.common.by import By
 import time
 
 
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    go_to_login_page(browser)
     time.sleep(3)
