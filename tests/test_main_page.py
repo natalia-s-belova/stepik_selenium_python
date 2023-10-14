7f50b68c392886cfe176b6f81de49a79030d8b57
natalia-s-belova <natalya.s.belova@gmail.com>
2023-10-14 14:18:21 +0400

    Move locators to a separate module

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -9,13 +9,6 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+    page.should_be_login_link()
     page.go_to_login_page()
     time.sleep(3)
-
-
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
